b8f72eb39ade577495057b40e13d80b1cbb381b7
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-06-02 14:52:46 -0300

    Progress on day 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,31 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
+app.use(express.json());
 
-  response.json([
-    {
-      nome: "lucas",
-    },
-    { nome: "thiago" },
-    { nome: "daniel" },
-  ]);
+const users = [{ nome: "lucas" }, { nome: "thiago" }, { nome: "daniel" }];
+
+app.get("/users", (request, response) => {
+  const search = String(request.query.search);
+  console.log("search", search);
+  const filteredUsers = search
+    ? users.filter((user) => user.nome.includes(search))
+    : users;
+  response.json(filteredUsers);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id];
+  response.json(user);
+});
+
+app.post("/users", (req, res) => {
+  const data = req.body;
+  const user = {
+    nome: data.name,
+  };
+  return res.json(user);
 });
 
 app.listen("3333");
