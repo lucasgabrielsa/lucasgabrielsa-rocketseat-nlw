b13948b12acf70667c64a5dc11e75d7f8c1eed41
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-06-02 16:45:56 -0300

    Add Migration with knex and sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-const users = [{ nome: "lucas" }, { nome: "thiago" }, { nome: "daniel" }];
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-  console.log("search", search);
-  const filteredUsers = search
-    ? users.filter((user) => user.nome.includes(search))
-    : users;
-  response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const user = users[id];
-  response.json(user);
-});
-
-app.post("/users", (req, res) => {
-  const data = req.body;
-  const user = {
-    nome: data.name,
-  };
-  return res.json(user);
-});
+app.use(routes);
 
 app.listen("3333");
