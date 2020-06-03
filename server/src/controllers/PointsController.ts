25e1ccaa93607e6161b7db16d9d7814adcbea005
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-06-03 08:49:59 -0300

    Changes to Points Controller

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,8 +3,21 @@ import knex from "../database/connection";
 
 class PointsController {
   async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    console.log(city, uf, items);
+    const parsedItems: Number[] = String(items)
+      .split(", ")
+      .map((item) => Number(item.trim()));
+
     await knex.transaction(async (trx) => {
-      const points = await trx("points").select("*");
+      const points = await trx("points")
+        .join("point_items", "points.id", "=", "point_items.point_id")
+        .whereIn("point_items.item_id", parsedItems)
+        .where("city", String(city))
+        .where("uf", String(uf))
+        .distinct()
+        .select("points.*");
       return response.json(points);
     });
   }
