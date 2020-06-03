86cc882c19eeed6464b615fb4124cac14152b584
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-06-03 18:27:26 -0300

    Styling the Create Point Page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 import { FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -18,12 +19,12 @@ const Home = () => {
             eficiente.
           </p>
 
-          <a href="cadastro">
+          <Link to="create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
